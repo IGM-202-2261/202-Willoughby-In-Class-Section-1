2a185804d5ab2b2549c4a11d53203a761315a12a
Austin Willoughby <arwigm@rit.edu>
2026-08-31 11:52:11 -0400

    Week 2 Monday

---
+++ 2D Project/Assets/Scripts/Week2Demo.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Week2Demo : MonoBehaviour
+{
+    private int counter = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector2 localPos = transform.localPosition;
+        //localPos.x += 0.01f;
+        //transform.localPosition = localPos;
+    }
+
+    private void OnMouseEnter()
+    {
+        
+    }
+}
