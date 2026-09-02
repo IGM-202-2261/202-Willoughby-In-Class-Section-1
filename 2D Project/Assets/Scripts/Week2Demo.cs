e3a50d9817b33afd838653c754439d445a13299e
Austin Willoughby <arwigm@rit.edu>
2026-09-02 11:50:38 -0400

    Wednesday Week 2

--- 2D Project/Assets/Scripts/Week2Demo.cs
+++ 2D Project/Assets/Scripts/Week2Demo.cs
@@ -4,10 +4,27 @@ public class Week2Demo : MonoBehaviour
 {
     private int counter = 0;
 
+    [SerializeField]
+    private SpriteRenderer sRend;
+
+    [SerializeField]
+    private Color overlapColor;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //Debug.Log("Hello World");
+        //sRend = GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        sRend.color = overlapColor;
+        collision.gameObject.GetComponent<SpriteRenderer>().color = Color.purple;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        sRend.color = Color.white;
     }
 
     // Update is called once per frame
@@ -18,8 +35,5 @@ public class Week2Demo : MonoBehaviour
         //transform.localPosition = localPos;
     }
 
-    private void OnMouseEnter()
-    {
-        
-    }
+
 }
